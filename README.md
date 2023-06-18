edf5094d2208b21233aaa7753a2774101b0e6ffd
cacosandon <jiossandon@uc.cl>
2023-06-18 14:13:59 -0400

    chore: add serpapi tool and use .env file

--- README.md
+++ README.md
@@ -6,14 +6,15 @@ Github-Agent is a test application that leverages [PyGithub](https://github.com/
 <video src="docs/demo.mov" alt="Demo video"></video>
 
 ## 🧪 Features
-- Search repositories by name
-- Get pull requests (PRs) and issues
+- Search repositories by name.
+- Get pull requests (PRs) and issues.
 - Access the body and collaborators of specific PRs or issues.
+- Search the internet to solve issues or search for projects.
 
 ## How to use
 1. `git clone https://github.com/cacosandon/github-agent/tree/main`
 
-2. Fill `OPENAI_API_KEY` and `GITHUB_TOKEN` (optional) in `main.py` with your tokens. Github token is not necessary. Adding your Github token allows you to increase the API limit rate and also give access to your repositories.
+2. Fill `OPENAI_API_KEY`, `GITHUB_TOKEN` (optional) and `SERPAPI_API_KEY` (optional) in a `.env` file with your tokens. Github token is not necessary. Adding your Github token allows you to increase the API limit rate and also give access to your repositories.
 
 3. Create an environment and install dependencies with `poetry shell && poetry install`
 
--- main.py
+++ main.py
@@ -1,23 +1,40 @@
+import os
+from dotenv import load_dotenv
+
 from langchain.tools import StructuredTool
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import initialize_agent, AgentType
+from langchain.utilities import SerpAPIWrapper
 
 from github_agent.tools.github_tool import GithubTools
 
-OPENAI_API_KEY = "your openai api key here"
-GITHUB_TOKEN = "your github token here"
+load_dotenv()
 
-github_tools = GithubTools(token=GITHUB_TOKEN)
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", "")
+SERPAPI_API_KEY = os.environ.get("SERPAPI_API_KEY")
 
+github_functions = GithubTools(token=GITHUB_TOKEN)
 tools = [
-    StructuredTool.from_function(github_tools.search_repositories),
-    StructuredTool.from_function(github_tools.get_repository_pull_requests),
-    StructuredTool.from_function(github_tools.get_repository_pull_request_information),
-    StructuredTool.from_function(github_tools.get_repository_issues),
-    StructuredTool.from_function(github_tools.get_repository_issue_information),
+    StructuredTool.from_function(github_functions.search_repositories),
+    StructuredTool.from_function(github_functions.get_repository_pull_requests),
+    StructuredTool.from_function(github_functions.get_repository_pull_request_information),
+    StructuredTool.from_function(github_functions.get_repository_issues),
+    StructuredTool.from_function(github_functions.get_repository_issue_information),
 ]
 
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, streaming=True, verbose=True, model="gpt-3.5-turbo")
+if SERPAPI_API_KEY:
+    tools += [
+        StructuredTool.from_function(SerpAPIWrapper().run, name="search"),
+    ]
+
+llm = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    temperature=0,
+    streaming=True,
+    verbose=True,
+    model="gpt-3.5-turbo",
+)
 
 agent = initialize_agent(
     tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True
